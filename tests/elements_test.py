24aaf9d328cc547185393e86b731df0098b9288a
takeso-user <takeso_kensai@hotmail.com>
2024-06-23 16:44:07 +0200

    small fixes under hood relating to readability

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -17,8 +17,12 @@ class TestElements:
         def test_text_box(self, driver):
             text_box_page = TextBoxPage(driver, url="https://demoqa.com/text-box")
             text_box_page.open()
-            input_data = text_box_page.fill_all_fields()
+            input_name, input_email, input_current_address, input_permanent_address = (
+                text_box_page.fill_all_fields())
+            output_name, output_email, output_current_address, output_permanent_address = (
+                text_box_page.check_filled_form_fields())
             time.sleep(2)
-            output_data = text_box_page.check_filled_form_fields()
-            time.sleep(2)
-            assert input_data == output_data
+            assert input_name == output_name, "The full name is not match"
+            assert input_email == output_email, "The email is not match"
+            assert input_current_address == output_current_address, "The current address is not match"
+            assert input_permanent_address == output_permanent_address, "The permanent address is not match"
